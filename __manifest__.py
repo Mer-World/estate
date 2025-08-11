7adbfacd956beaaddb7b70919f468b0c68752cbb
Meronawit Dejene <150444100+Mer-World@users.noreply.github.com>
2025-08-11 14:52:36 +0300

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'depends': ['base'],
+    'author': 'Meronawit Dejene',
+    'category': 'Uncategorized',
+    'summary': 'A simple real estate module',
+    'description': 'This module manages real estate properties.',
+    'installable': True,
+    'application': True,  
+    
+}
